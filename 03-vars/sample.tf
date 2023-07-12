cb50f9862a93e0010888798d748350afa6006697
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-12 17:01:33 +0530

    sdc

--- 03-vars/sample.tf
+++ 03-vars/sample.tf
@@ -1,6 +1,6 @@
 
 
-variable "city" {}           // giving empty varible but it will be pickup from the terraform.tfvars is its the defaullt one
+variable "city" {}           // giving empty varible but it will be pickup from the terraform.tfvars is its the defaullt one and city is the varible name so we will give in the outpust section
 
 output "city_name" {                          // using this variable we can print the city name from th eterraform.tfvars
     value = "Our city name is ${var.city}"
