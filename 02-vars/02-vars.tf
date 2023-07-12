22c4e4a97884e2c4ae1d07c6022734db4d00aeb7
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-12 15:48:19 +0530

    sdc

--- 02-vars/02-vars.tf
+++ 02-vars/02-vars.tf
@@ -1,18 +1,18 @@
-variable "sample" {
-    default = "Santhosh Welcome To Terraform"
-}
+#variable "sample" {
+#    default = "Santhosh Welcome To Terraform"
+#}
 
-output "sample" {
-    default = "Santhosh Welcome To Terraform training"
-}
+#output "sample" {
+#    default = "Santhosh Welcome To Terraform training"
+#}
 
-variable "sample_op" {
-    value = "Santhosh Welcome To Terraform training"
-}
+#variable "sample_op" {
+#    value = "Santhosh Welcome To Terraform training"
+#}
 
-output "sample_var_op" {
-    value = "Value of the variable is ${sample_var_op}"
-}
+#output "sample_var_op" {
+#    value = "Value of the variable is ${sample_var_op}"
+#}
 
 
 
@@ -28,6 +28,8 @@ variable "example_list" {
         "TERAFORM" 
     ]
 }
+
+
 
 output "example_list_op" {
     value = "Welcome to ${var.example_list[1]}  with ${var.example_list[0]} Training and this is our batch ${var.example_list[2]} and the current topic is ${var.example_list[4]}"
