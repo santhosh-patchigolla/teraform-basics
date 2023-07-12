9aaa63d9f7df9dee7bbd4bd4ef60b27303edc842
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-12 16:31:40 +0530

    sdc

--- 02-vars/02-vars.tf
+++ 02-vars/02-vars.tf
@@ -15,7 +15,7 @@
     }
 
 
-# variable "test" {}  // Empty Variable
+# variable "test" {}   > // Empty Variable
 
  #A Variable can be accessed directly by calling it as var.variableName, but if you're accessing in between a set of strings, then you need to enclose them in quotes and it should referred using ${var.VarName},#In Terraform there is no concept of Single Quotes. 
  #below is the varible list 
