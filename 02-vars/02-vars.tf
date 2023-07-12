4624ba5ebb182923cc9042f053683440acc9f54b
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-12 13:31:23 +0530

    sdc

--- 02-vars/02-vars.tf
+++ 02-vars/02-vars.tf
@@ -5,3 +5,24 @@ variable "sample" {
 output "sample-op" {
     value = var.sample
 }
+
+
+variable "sample_op" {
+    default = "Welcome to terraform training"
+}
+
+# below is the varible list 
+
+variable "example_list" {
+    default = [
+        "AWS",
+        "DEVOPS",
+         55,
+        "TRAINER",
+        "TERAFORM" 
+    ]
+}
+
+output "example_list_op" {
+    value = Welocome to ${var.example_list[1]} with ${var.exaple_list[0]} and this is batch of ${var.exaple_list[3]} and the currenc topic is ${var.example_list[4]}
+}
