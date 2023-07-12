fa74813fecc698dde83fdba683a6dc6f10a9c365
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-12 16:58:03 +0530

    sdc

--- 03-vars/sample.tf
+++ 03-vars/sample.tf
@@ -1,1 +1,14 @@
-city = "Vizag"
+
+
+variable "city" {}           // giving empty varible but it will be pickup from the terraform.tfvars is its the defaullt one
+
+output "city_name" {
+    value = "Our city name is ${var.city}"
+}  
+
+
+
+
+
+
+
