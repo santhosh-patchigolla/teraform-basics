57a23832f7c549c13185642c5d489af0557db072
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-13 12:57:02 +0530

    sdc

--- sample/ec2.tf
+++ sample/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "web" {                 // this will create the ec2 instance and you neeed to give the provider name(nothing but the cloud) you can give below or give in the seperate file provider.tf
   ami           = "ami-0b374f36561bf0fd4"
   instance_type = "t3.micro"
 
