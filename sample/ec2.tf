9c5aef2252fe76c50381ef784ec3e9477a4c8f72
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-13 12:55:05 +0530

    sdc

--- sample/ec2.tf
+++ sample/ec2.tf
@@ -1,7 +1,8 @@
-resource "aws_instance" "web" {                         
+resource "aws_instance" "web" {
+  ami           = "ami-0b374f36561bf0fd4"
   instance_type = "t3.micro"
 
   tags = {
     Name = "HelloWorld"
   }
-}                                          
+}
