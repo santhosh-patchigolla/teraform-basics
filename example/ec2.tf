bed1f06a564000dc2c64be0d5d1f6ad67d135929
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-17 13:59:25 +0530

    sdc

--- example/ec2.tf
+++ example/ec2.tf
@@ -21,7 +21,7 @@ resource "aws_security_group" "allow_all" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
