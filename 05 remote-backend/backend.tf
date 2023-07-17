275aa9d94ae8c502ff5f06ec52c90056f2ed7836
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-17 12:47:08 +0530

    sdc

--- 05 remote-backend/backend.tf
+++ 05 remote-backend/backend.tf
@@ -3,7 +3,7 @@
 terraform {
   backend "s3" {
     bucket = "b54-santhosh-terraform-remote-state"
-    key    = "05-remote/terraform.tfstate"                    # this is the you need to give the backend path means where we store the state file
+    key    = "05-remote/terraform.tfstate"                    # this is the you need to give the backend path means where we store the state file,terrafrom.tfstate you can see in the s3 bucket where the state file is located.
     region = "us-east-1"
   }
 }
