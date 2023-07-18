da3449e0b2d210eee8cd8a2739e492e8272c3ea9
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-18 15:18:16 +0530

    sdc

--- 07-modules/ec2/main.tf
+++ 07-modules/ec2/main.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "sample" {                 // this will create the ec2 instance and you neeed to give the provider name(nothing but the cloud) you can give below or give in the seperate file provider.tf
-  ami                    = "ami-0b374f36561bf0fd4"
+  ami                    = data.aws_ami.image.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [var.sg]                           // you can get the security group attributes from the document 
 }
@@ -10,3 +10,4 @@ variable "sg" {}
 output "public_ip"{
     value   = aws_instance.sample.public_ip
 }
+
