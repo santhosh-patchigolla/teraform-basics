cf45eb9d5eba243d838063d3e7b38ff9386d08a2
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-18 15:28:53 +0530

    sdc

--- 07-modules/ec2/data.tf
+++ 07-modules/ec2/data.tf
@@ -1,5 +1,5 @@
 data "aws_ami" "image" {   // using this we can fetch the latest AMI of the ansilbe-lab-image
   most_recent      = true
-  name_regex       = "ansible-lab-image"
-  owners           = ["self"]
+  name_regex       = "DevOps-LabImage-CentOS7"
+  /* owners           = ["self"]                   // used self because this {ansible-lab-image} ami is in my account if it is other than your account you can remove the self once. */
 }
