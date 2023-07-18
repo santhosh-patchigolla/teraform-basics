719309c67d2fcddc9075afbf4e967ae4aaf54748
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-18 15:21:08 +0530

    sdc

--- 07-modules/ec2/data.tf
+++ 07-modules/ec2/data.tf
@@ -1,4 +1,5 @@
 data "aws_ami" "image" {   // using this we can fetch the latest AMI of the ansilbe-lab-image
   most_recent      = true
   name_regex       = "ansible-lab-image"
+  owners           = ["self"]
 }
