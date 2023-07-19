47127a65cc563af52ff7b7449a0e3208ff27edcc
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-19 13:29:14 +0530

    sdc

--- 07-modules/ec2/main.tf
+++ 07-modules/ec2/main.tf
@@ -1,5 +1,5 @@
 
-resource "aws_instance" "sample" {                 // this will create the ec2 instance and you neeed to give the provider name(nothing but the cloud) you can give below or give in the seperate file provider.tf
+resource "aws_instance" "samplee" {                 // this will create the ec2 instance and you neeed to give the provider name(nothing but the cloud) you can give below or give in the seperate file provider.tf
   ami                    = data.aws_ami.image.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [var.sg]                           // you can get the security group attributes from the document 
