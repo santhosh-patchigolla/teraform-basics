f59338e84ac2c54b317ab9f36306025a493cae31
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-19 11:30:06 +0530

    sdc

--- 05remote-backend/ec2.tf
+++ 05remote-backend/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "remote-app" {                 // this will create the ec2 instance and you neeed to give the provider name(nothing but the cloud) you can give below or give in the seperate file provider.tf
+resource "aws_instance" "remote-app" {                 // this will create the ec2 instance and you neeed to give the provider name(nothing but the cloud) you can give below or give in the seperate file provider.tf and this "remote-app" is a name where terrafrom can recognise the sever ID and Tag name is some thing that the server will be spin with the same ID
   ami           = "ami-0b374f36561bf0fd4"
   instance_type = "t2.micro"
 
