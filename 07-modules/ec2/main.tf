d095d98b84fe23347d8642a516357006417b6d23
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-19 12:54:51 +0530

    sdc

--- 07-modules/ec2/main.tf
+++ 07-modules/ec2/main.tf
@@ -3,6 +3,22 @@ resource "aws_instance" "sample" {                 // this will create the ec2 i
   ami                    = data.aws_ami.image.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [var.sg]                           // you can get the security group attributes from the document 
+
+# This will excute on the top of the remote server once it got created
+  provisioner "remote-exec" {
+
+# Below is the connection block where we can pass the creds of the remote server to tun the playbook
+  connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = self.private_ip   # as we dont know the IP because the server hasn't created yet we can use host section refer from doc
+  }    
+  
+    inline = [
+      "ansible-pull -U https://github.com/santhosh-patchigolla/ansible.git playbookName.yml -e ENV=dev -e COMPONENT=mongodb",      
+    ]
+  }
 }
 
 variable "sg" {}
