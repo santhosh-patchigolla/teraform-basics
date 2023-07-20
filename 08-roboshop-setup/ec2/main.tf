1e0662f596a5bd3a9a2391d67d79dad2c6f2ef23
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-07-20 11:23:35 +0530

    sdc

--- 08-roboshop-setup/ec2/main.tf
+++ 08-roboshop-setup/ec2/main.tf
@@ -3,6 +3,7 @@ resource "aws_spot_instance_request" "spot_worker" {
   ami                         = "data.aws_ami.image.id"
   instance_type               = "t2.micro"
   vpc_security_group_ids      = [aws_security_group.allows_all.id]
+  wait_for_fulfillment        =  true
 
   tags = {
     Name = var.COMPONENT
